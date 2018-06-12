eaca09e067a4149d55783c7b0c3e095dc2423c3f
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2018-06-12 18:14:51 +0530

    [content] add insta link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-import github from '../img/github-icon.svg'
+import instagram from '../img/instagram-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = () => (
@@ -10,7 +10,7 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={logo} alt="rickypedia.life" style={{ width: '88px' }} />
           </figure>
         </Link>
       </div>
@@ -18,19 +18,16 @@ const Navbar = () => (
         <Link className="navbar-item" to="/about">
           About
         </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
       </div>
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://www.instagram.com/rickypedia.life/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <span className="icon">
-            <img src={github} alt="Github" />
+            <img src={instagram} alt="Instagram" />
           </span>
         </a>
       </div>
